5485f3df1eca1a8baa14a2d911c20a393dc31a5c
bhthong20 <122037194+bhthong20@users.noreply.github.com>
2023-09-20 21:59:38 +0700

    hihi

--- WebBanGiay/src/main/java/com/example/demo/controllers/TestController.java
+++ WebBanGiay/src/main/java/com/example/demo/controllers/TestController.java
@@ -1,9 +1,31 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.ChatLieu;
+import jakarta.persistence.ManyToOne;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    @GetMapping("/hien-thi")
+    public String hienThi(Model model, @RequestParam("num") Optional<Integer> num,
+                          @RequestParam(name = "size", defaultValue = "5", required = false) Integer size) {
+        Sort sort = Sort.by("ngayTao").descending();
+        Pageable pageable = PageRequest.of(num.orElse(0), size, sort);
+        Page<ChatLieu> list = no;
+        model.addAttribute("listChatLieu", list.getContent());
+        model.addAttribute("total", list.getTotalPages());
+        return "chat-lieu/hien-thi";
+    }
 }
